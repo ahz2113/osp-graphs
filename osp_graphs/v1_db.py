c705277b174bc129a7f688655d3157d02062f83d
Amy Zhang <30630940+ahz2113@users.noreply.github.com>
2018-08-21 12:29:03 -0400

    updated v1_db.py
    updated version from original v1_db.py in the opensyllabus/osp-graphs repo

---
+++ osp_graphs/v1_db.py
@@ -0,0 +1,31 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.engine.url import URL
+from sqlalchemy.ext.automap import automap_base
+
+
+# TODO: Config-ify.
+url = URL(drivername='postgresql', database='osp_graphs')
+
+engine = create_engine(url)
+
+factory = sessionmaker(bind=engine)
+
+session = scoped_session(factory)
+
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+Text = Base.classes.text
+Citation = Base.classes.citation
+Field = Base.classes.field
+Subfield = Base.classes.subfield
+SubfieldDocument = Base.classes.subfield_document
+Document = Base.classes.document
+
+# added after original code:
+Institution = Base.classes.institution
+InstitutionDocument = Base.classes.institution_document
